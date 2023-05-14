86e780df3d7972a5c7d62c604884ae5531bec09f
JankeRyb <125120471+JankeRyb@users.noreply.github.com>
2023-05-14 19:56:16 +0200

    zmiana2

--- PESEL.py
+++ PESEL.py
@@ -5,9 +5,16 @@ import input_exceptions
 
 birth_year=input_exceptions.birth_year_input('Input your birthdate - Year (1800 - 2299): ')
 print(birth_year)
-
+if input_exceptions.CheckLeap(True):
+    print("True")
+else:
+    print("False")
 birth_month=input_exceptions.birth_month_input('Input your birthdate - Month (1 - 12): ')
+
+
 birth_day=input_exceptions.birth_day_input('Input your birthdate - Day (1 - 31): ')
+
+
 gender = input_exceptions.gender_input('Male/Female - M/F: ')
 # PESEL RRMMDDLLLLK
 # #Losowe cyfry - L
--- input_exceptions.py
+++ input_exceptions.py
@@ -1,7 +1,20 @@
+def CheckLeap(Year):
+  # Checking if the given year is leap year
+  if((Year % 400 == 0) or
+     (Year % 100 != 0) and
+     (Year % 4 == 0)):
+    print("Given Year is a leap Year")
+
+  # Else it is not a leap year
+  else:
+    print ("Given Year is not a leap Year")
+# Taking an input year from user
+
 def birth_year_input(message):
     while True:
         try:
             birth_year = int(input(message))
+            CheckLeap(birth_year)
             assert 1800 <= birth_year <= 2299
             return birth_year
         except ValueError:
@@ -13,14 +26,15 @@ def birth_month_input(message):
     while True:
         try:
             birth_month = int(input(message))
-            assert 10 <= birth_month <= 12
+            assert 1 <= birth_month <= 12
             return birth_month
         except ValueError:
             print("Not a valid number, try again")
         except AssertionError:
-            print('Year MUST BE between 1800 and 2299')
+            print('Month MUST BE between 1 and 12')
 
 #czy robimy test na rok przestepny i luty 28/29 dni???
+
 
 def birth_day_input(message):
     while True:
@@ -31,7 +45,7 @@ def birth_day_input(message):
         except ValueError:
             print("Not a valid number, try again")
         except AssertionError:
-            print('Year MUST BE between 1 and 31')
+            print('Day MUST BE between 1 and 31')
 
 def gender_input(message):
     while True:

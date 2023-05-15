4e4f6834d5b3f47c728ddcd9f897d98b76bedaa3
JankeRyb <125120471+JankeRyb@users.noreply.github.com>
2023-05-15 21:09:13 +0200

    zmiana3

--- PESEL.py
+++ PESEL.py
@@ -22,9 +22,69 @@ else:
     birth_day=input_exceptions.birth_day_input_31('Input your birthdate - Day (1 - 31): ')
 
 
-
-
 gender = input_exceptions.gender_input('Male/Female - M/F: ')
+
+
+if gender == "M":
+    test_list=[1,3,5,7,9]
+    gender_random = random.sample(test_list,1)
+else:
+    test_list=[0,2,4,6,8]
+    gender_random = random.sample(test_list,1)
+
+
+year = '%02d' % (birth_year % 100)
+month = '%02d' % birth_month
+day = '%02d' % birth_month
+
+
+three_random = random.randint(100,999)
+three_random = str(three_random)
+
+a = year[0]
+a = int(a)
+
+b = year[1]
+b = int(b)
+
+c = month[0]
+c = int(c)
+
+d = month[1]
+d = int(d)
+
+e = day[0]
+e = int(e)
+
+f = day[1]
+f = int(f)
+
+g = three_random[0]
+g = int(g)
+
+h = three_random[1]
+h = int(h)
+
+i = three_random[2]
+i = int(i)
+
+j = gender_random[0]
+j = int(j)
+
+
+
+check = a + 3 * b + 7 * c + 9 * d + e + 3 * f + 7 * g + 9 * h + i + 3 * j
+if check % 10 == 0:
+    last_digit = 0
+else:
+    last_digit = 10 - (check % 10)
+
+print('%02d' % (birth_year % 100), end='')
+print('%02d' % birth_month, end='')
+print('%02d' % birth_day, end='')
+print(three_random, end='')
+print(j, end='')
+print(last_digit)
 # PESEL RRMMDDLLLLK
-# #Losowe cyfry - L
+# Losowe cyfry - L
 # Cyfra kontrolna - K
